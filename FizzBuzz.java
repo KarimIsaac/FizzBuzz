c931da4016bca306761185ebff4c394de6a5cad7
KarimIsaac <90776600+KarimIsaac@users.noreply.github.com>
2022-09-04 09:04:45 -1200

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,23 @@
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+  
+    
+  
+      for (int i = 0; i <= 100; i++) {
+  
+        if (i == 42) {
+          System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything");
+        } else if (i % 5 == 0 && i % 3 != 0) {
+          System.out.println("Buzz");
+        } else if (i % 3 == 0 && i % 5 == 0) {
+          System.out.println("Fizzbuzz");
+        } else if (i % 3 == 0 && i % 5 != 0) {
+          System.out.println("Fizz");
+        } else {
+          System.out.println(i);
+        }
+  
+      }
+    }
+  }
